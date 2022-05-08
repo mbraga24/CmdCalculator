281e9f722ea473de5019cc090562918026017e7b
mrdbrg <rdg.brg@gmail.com>
2022-05-08 19:08:03 -0400

    better java programmer

---
+++ src/com/havefunwith/CmdCalculator/Main.java
@@ -0,0 +1,10 @@
+package com.havefunwith.CmdCalculator;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("\n===================================================");
+        System.out.println("Hey, you! It's the command line calculator project.");
+        System.out.println("===================================================\n");
+    }
+}
