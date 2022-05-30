769523a4fa1326e0b57839e1cf36bfba17cb0403
mrdbrg <rdg.brg@gmail.com>
2022-05-30 14:46:43 -0400

    set up MathEquation class and creating instances

--- src/com/havefunwith/CmdCalculator/Main.java
+++ src/com/havefunwith/CmdCalculator/Main.java
@@ -15,30 +15,13 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-
-        performCalculations();
-
-       /*
-           When a user passes command-line arguments they come as parameter to
-           the Main method, they come in as args parameter.
-
-            => If user provides no command-line arguments the app will process the default operation.
-
-            => If user provides the correct number of command-line arguments:
-                - opCode - leftVal - rightVal
-               The application will do a calculation using the command-line arguments.
-         */
         System.out.println("\n===================================================");
         System.out.println("It's the command line calculator project.");
         System.out.println("===================================================\n");
+        performCalculations();
     }
 
     private static void performCalculations() {
-        /*
-         * Parallel arrays -
-         * Elements in each array are meant to be used with the corresponding element in each of
-         * the other arrays.
-         */
         double[] leftValues = {100.0, 25.0, 225.0, 11.0};
         double[] rightValues = {50.0, 92.0, 17.0, 3.0};
         char[] opCodes = {'d', 'a', 's', 'm'};
@@ -50,8 +33,13 @@ public class Main {
             MathEquation class, instead 4 references of type MathEquation are being created. Each element
             within this array needs to be explicitly create an instance of the MathEquation class.
          */
-        MathEquation[] equations = MathEquation[4];
+        MathEquation[] equations = new MathEquation[4];
+
+        // Each of the elements in the array are referencing new instance of the MathEquations class.
         equations[0] = create(100.0, 50.0, 'd');
+        equations[1] = create(25.0, 92.0, 'a');
+        equations[2] = create(225.0, 17.0, 's');
+        equations[3] = create(11.0, 3.0, 'm');
 
         // Each of the fields within MathEquation have their appropriate values.
         // equations[0] = new MathEquation();
@@ -61,23 +49,12 @@ public class Main {
 
         equations[0] = create(100.0, 50.0, 'd');
 
-        // if the length is 0, there is no command-line arguments.
         if (args.length == 0) {
-            /*
-             * Traditional For loop -
-             * Allows incrementing index and access each individual elements across each of the arrays.
-             */
+
             for (int i = 0; i < opCodes.length; i++) {
                 results[i] = execute(opCodes[i], leftValues[i], rightValues[i]); // Pass the appropriate members of each of the arrays as arguments
             }
 
-            /*
-             * For each loop -
-             * The results are all in one array, thus For each loop can be used to loop over each array element.
-             *
-             * For each loop will still need to have the variable initialized with the proper type to return each
-             * array element.
-             */
             for (double currentResult : results) {
                 System.out.println(currentResult);
             }
@@ -109,19 +86,8 @@ public class Main {
      */
     static void executeInteractively() {
         System.out.println("Enter an operation and two numbers");
-        /*
-            When instantiating Scanner, System.in is passed into Scanner constructor.
-            This instance of Scanner will take care of the input given by the user.
-         */
         Scanner scanner = new Scanner(System.in);
-        /*
-            .nextLine() retrieves that input stored from the user as a String.
-         */
         String userInput = scanner.nextLine();
-        /*
-            .split() method accepts an expression to identify what needs to be used to split
-            the string into parts.
-         */
         String[] parts = userInput.split(" ");
         performFromOperation(parts);
     }
@@ -176,27 +142,7 @@ public class Main {
     }
 
     private static void handleCommandLineArgs(String[] args) {
-            /*
-                Getting the data from the command-line will require a few steps.
-
-                Remember: Java is a strongly typed language. The arguments are coming
-                in as Strings. Each individual String is considered a sequence of characters.
-             */
-
-            /*
-                args[0].charAt(0) -
-                    - Retrieve the first argument of the String array.
-                    - Because a String is considered a sequence of characters, extract the
-                        first character from it.
-
-                This line is converting a String representation of a character, into a char representation of a character.
-             */
         char opCode = args[0].charAt(0);
-            /*
-                Double.parseDouble(args[1]) -
-                Java provides a class named Double that has a utility method that named .parseDouble
-                that converts a sequence of character into a double (pass args[1] & args[2] - args sub 1 - into method)
-             */
         double leftVal = Double.parseDouble(args[1]);
         double rightVal = Double.parseDouble(args[2]);
         double result = execute(opCode, leftVal, rightVal);
@@ -210,18 +156,6 @@ public class Main {
      */
     private static void displayResult(char opCode, double leftVal, double rightVal, double result) {
         char symbol = symbolFromOpCode(opCode);
-        /*
-        StringBuilder builder = new StringBuilder(20);
-        builder.append(leftVal);
-        builder.append(" ");
-        builder.append(symbol);
-        builder.append(" ");
-        builder.append(rightVal);
-        builder.append(" = ");
-        builder.append(result);
-        String output = builder.toString();
-        **/
-
         String output = String.format("%.3f %c %.3f = %.3f", leftVal, symbol, rightVal, result);
         System.out.println(output); // Keep in mind that StringBuilder it's not itself a String. It needs to be assigned to a String variable.
     }
@@ -230,7 +164,6 @@ public class Main {
         Translate the opCode into the appropriate mathematical symbol.
      */
     private static char symbolFromOpCode(char opCode) {
-        // Parallel arrays
         char[] opCodes = {'a', 's', 'm', 'd'};
         char[] symbols = {'+', '-', '*', '/'};
         char symbol = ' ';
@@ -263,28 +196,13 @@ public class Main {
                 "zero", "one", "two", "three", "four", "five",
                 "six", "seven", "eight", "nine"
         };
-        /*
-            Local variable will be initially set to -1 instead of 0.
-            This starts the local variable with a value that isn't valid. At the end of the for-loop, if
-            the value still set to -1, that means that the application never matched any of the words the
-            application understands.
-         */
-//        double value = 0;
         double value = -1;
-        /*
-            When the word is matched on the conditional statement, break exists the loop and then runs the
-            next statement after the loop.
-         */
         for (int index = 0; index < numberWords.length; index++) {
             if (word.equals(numberWords[index])) {
                 value = index;
                 break;
             }
         }
-        /*
-            Parse number out of the String.
-            The primitive wrapper class Double will assist on this task.
-         */
         if (value == -1) {
             value = Double.parseDouble(word);
         }
